dc49f2574ae365e84ffef6b3ccb84dc25469a3fe
sejun2 <qpfjf56@gmail.com>
2022-01-05 19:50:32 +0900

    stash

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -36,99 +36,123 @@ class MainPage extends GetView<MainPageController> {
 
   buildChips(BuildContext context) {
     return Wrap(
-      children:  [
+      children: [
         InkWell(
           borderRadius: BorderRadius.all(Radius.circular(16)),
-          onTap: (){},
+          onTap: () {},
           child: Padding(
             padding: const EdgeInsets.all(8.0),
             child: Chip(
               elevation: 3,
-              label: Text('Chip', style: TextStyle(color: Colors.white),),
+              label: Text(
+                'Chip',
+                style: TextStyle(color: Colors.white),
+              ),
               backgroundColor: Colors.blue,
             ),
           ),
         ),
         InkWell(
           borderRadius: BorderRadius.all(Radius.circular(16)),
-          onTap: (){},
+          onTap: () {},
           child: Padding(
             padding: const EdgeInsets.all(8.0),
             child: Chip(
               elevation: 3,
-              label: Text('Chip', style: TextStyle(color: Colors.white),),
+              label: Text(
+                'Chip',
+                style: TextStyle(color: Colors.white),
+              ),
               backgroundColor: Colors.blue,
             ),
           ),
         ),
         InkWell(
           borderRadius: BorderRadius.all(Radius.circular(16)),
-          onTap: (){},
+          onTap: () {},
           child: Padding(
             padding: const EdgeInsets.all(8.0),
             child: Chip(
               elevation: 3,
-              label: Text('Chip', style: TextStyle(color: Colors.white),),
+              label: Text(
+                'Chip',
+                style: TextStyle(color: Colors.white),
+              ),
               backgroundColor: Colors.blue,
             ),
           ),
         ),
         InkWell(
           borderRadius: BorderRadius.all(Radius.circular(16)),
-          onTap: (){},
+          onTap: () {},
           child: Padding(
             padding: const EdgeInsets.all(8.0),
             child: Chip(
               elevation: 3,
-              label: Text('Chip', style: TextStyle(color: Colors.white),),
+              label: Text(
+                'Chip',
+                style: TextStyle(color: Colors.white),
+              ),
               backgroundColor: Colors.blue,
             ),
           ),
         ),
         InkWell(
           borderRadius: BorderRadius.all(Radius.circular(16)),
-          onTap: (){},
+          onTap: () {},
           child: Padding(
             padding: const EdgeInsets.all(8.0),
             child: Chip(
               elevation: 3,
-              label: Text('Chip', style: TextStyle(color: Colors.white),),
+              label: Text(
+                'Chip',
+                style: TextStyle(color: Colors.white),
+              ),
               backgroundColor: Colors.blue,
             ),
           ),
         ),
         InkWell(
           borderRadius: BorderRadius.all(Radius.circular(16)),
-          onTap: (){},
+          onTap: () {},
           child: Padding(
             padding: const EdgeInsets.all(8.0),
             child: Chip(
               elevation: 3,
-              label: Text('Chip', style: TextStyle(color: Colors.white),),
+              label: Text(
+                'Chip',
+                style: TextStyle(color: Colors.white),
+              ),
               backgroundColor: Colors.blue,
             ),
           ),
         ),
         InkWell(
           borderRadius: BorderRadius.all(Radius.circular(16)),
-          onTap: (){},
+          onTap: () {},
           child: Padding(
             padding: const EdgeInsets.all(8.0),
             child: Chip(
               elevation: 3,
-              label: Text('Chip', style: TextStyle(color: Colors.white),),
+              label: Text(
+                'Chip',
+                style: TextStyle(color: Colors.white),
+              ),
               backgroundColor: Colors.blue,
             ),
           ),
         ),
         InkWell(
           borderRadius: BorderRadius.all(Radius.circular(16)),
-          onTap: (){},
+          onTap: () {},
           child: Padding(
             padding: const EdgeInsets.all(8.0),
             child: Chip(
               elevation: 3,
-              label: Text('Chip', style: TextStyle(color: Colors.white),),
+              label: Text(
+                'Chip',
+                style: TextStyle(color: Colors.white),
+              ),
               backgroundColor: Colors.blue,
             ),
           ),
@@ -140,14 +164,26 @@ class MainPage extends GetView<MainPageController> {
   buildContent(BuildContext context) {
     return Expanded(
       child: Container(
-        padding: EdgeInsets.only(left: 14, right: 14, top: 8),
+        padding: const EdgeInsets.only(left: 14, right: 14, top: 8),
         width: Get.width,
         child: SingleChildScrollView(
           child: Column(
             children: [
-              SejunCard(labelText: 'THIS IS LABEL',),
-              SejunCard(labelText: 'THIS IS LABEL',),
-              SejunCard(labelText: 'THIS IS LABEL',),
+              SejunCard(
+                title: 'TITLE',
+                content: 'CONTENT',
+                labelText: 'THIS IS LABEL',
+              ),
+              SejunCard(
+                title: 'TITLE',
+                content: 'CONTENT',
+                labelText: 'THIS IS LABEL',
+              ),
+              SejunCard(
+                title: 'TITLE',
+                content: 'CONTENT',
+                labelText: 'THIS IS LABEL',
+              ),
             ],
           ),
         ),
@@ -157,11 +193,22 @@ class MainPage extends GetView<MainPageController> {
 
   buildAppBar(BuildContext context) {
     return AppBar(
+      leading: Material(
+        elevation: 2,
+        child: ClipRRect(
+          borderRadius: BorderRadius.all(Radius.circular(54)),
+          child: Image.network(
+              "https://dummyimage.com/600x400/000/fff", fit: BoxFit.fitWidth, width: 24, height: 24,),
+        ),
+      ),
       toolbarHeight: _appBarHeight,
       elevation: 4,
       centerTitle: true,
       title: const Text('Sejun'),
       shape: const StadiumBorder(),
+      actions: [
+        IconButton(onPressed: () {}, icon: Icon(Icons.g_translate)),
+      ],
     );
   }
 }
--- lib/widgets/sejun_card.dart
+++ lib/widgets/sejun_card.dart
@@ -1,10 +1,21 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class SejunCard extends StatefulWidget {
-  const SejunCard({Key? key, required this.labelText}) : super(key: key);
+  const SejunCard(
+      {Key? key,
+      required this.labelText,
+      this.imagePath = "https://dummyimage.com/1200x400/000/fff",
+      required this.title,
+      required this.content})
+      : super(key: key);
 
   final String labelText;
+  final String imagePath;
+  final String title;
+  final String content;
 
   @override
   _SejunCardState createState() => _SejunCardState();
@@ -15,7 +26,7 @@ class _SejunCardState extends State<SejunCard> {
   Widget build(BuildContext context) {
     return Stack(children: [
       Card(
-         margin: const EdgeInsets.only(top: 14, bottom: 16),
+          margin: const EdgeInsets.only(top: 14, bottom: 16),
           color: Colors.white,
           elevation: 4,
           shape: const RoundedRectangleBorder(
@@ -27,6 +38,27 @@ class _SejunCardState extends State<SejunCard> {
               mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
+                Expanded(
+                    flex: 4,
+                    child: ClipRRect(
+                      borderRadius: const BorderRadius.only(
+                          topLeft: Radius.circular(32),
+                          topRight: Radius.circular(32)),
+                      child: Container(
+                        width: Get.width,
+                        child: Image.network(
+                          widget.imagePath,
+                          fit: BoxFit.fill,
+                        ),
+                      ),
+                    )),
+                Container(
+                  width: Get.width,
+                  height: 1,
+                  color: Colors.grey,
+                ),
+                Expanded(flex: 1, child: Text(widget.title)),
+                Expanded(flex: 1, child: Text(widget.content))
               ],
             ),
           )),
@@ -34,7 +66,7 @@ class _SejunCardState extends State<SejunCard> {
           top: 0,
           left: 0,
           child: Chip(
-           backgroundColor: Colors.blueGrey,
+              backgroundColor: Colors.blueGrey,
               labelStyle: TextStyle(color: Colors.white),
               label: Text('${widget.labelText}')))
     ]);
